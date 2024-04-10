0b159ad46f6c18c5d16a2b940c6a576736198d9c
JoseRabal <jrabal.segura@gmail.com>
2024-04-10 11:37:47 +0200

    mui added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { GestionApp } from './GestionApp'
 import { BrowserRouter } from 'react-router-dom';
 import './styles.css'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
