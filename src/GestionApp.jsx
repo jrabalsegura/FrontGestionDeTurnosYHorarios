415414e44a5ec553fde8ca1662649fa5d93cdb18
JoseRabal <jrabal.segura@gmail.com>
2024-04-10 11:15:03 +0200

    First configs in app

---
+++ src/GestionApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const GestionApp = () => {
+  return (
+    <>
+      <h1>GestionApp</h1>
+    </>
+  );
+}
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { GestionApp } from './GestionApp'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GestionApp />
   </React.StrictMode>,
 )
